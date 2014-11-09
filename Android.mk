04f1071b81409dfe0909d1d5c54173bd5c9f172f
Ethan Chen <intervigil@gmail.com>
2014-11-08 23:27:23 -0800

    display: Use project pathmap
    Change-Id: Iabcd263ecf9f4eb28f4cd53862ba663c81275c10

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(call my-dir),$(call project-path-for,qcom-display))
+
 # This flag will be set to true during migration to Snapdragon Display Engine.
 TARGET_USES_SDE = false
 
@@ -17,4 +19,6 @@ else
 ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,$(display-hals))
 endif
+endif
+
 endif
